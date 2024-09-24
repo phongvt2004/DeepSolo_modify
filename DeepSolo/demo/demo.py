130edba9e30a4b2c0dff5685579639bc352ebc4c
phonghocode <ntp11892vt@gmail.com>
2024-09-25 03:27:34 +0700

    update

--- DeepSolo/demo/demo.py
+++ DeepSolo/demo/demo.py
@@ -84,7 +84,7 @@ def get_all_keyframes(root_dir):
     return all_keyframe_paths
 
 if __name__ == "__main__":
-    BATCH_SIZE = 6
+    BATCH_SIZE = 4
     mp.set_start_method("spawn", force=True)
     args = get_parser().parse_args()
     logger = setup_logger()
